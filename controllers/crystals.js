607560e25177794c969d59f96ba6d9e23013509b
alondra 3 <not given>
2021-09-30 18:07:41 -0700

    view all crystals, show page for one crystal, add new crystal

--- controllers/crystals.js
+++ controllers/crystals.js
@@ -2,8 +2,44 @@ const Crystal = require('../models/crystal');
 
 module.exports = {
     index,
-}
+    new: newCrystal,
+    create,
+    show,
+};
 
-function index (req, res) {
-    res.render('crystals/home');
-}
+function index(req, res) {
+    Crystal.find({}, function(err, crystalDocuments){
+        res.render('crystals/home', {
+            crystals: crystalDocuments,
+        });
+    });
+};
+
+function newCrystal(req,res) {
+    res.render('crystals/new', {
+        title: 'New Crystal',
+    });
+};
+
+function create(req,res) {
+    Crystal.create(req.body, function(err, createdCrystal) {
+        if(err) {
+            console.log(err);
+            return res.redirect('/crystals/new');
+        };
+    console.log(createdCrystal, '< crystal added by user');
+    res.redirect('/crystals')
+    });
+};
+
+async function show(req, res) {
+    try {
+        const crystal = await Crystal.findById(req.params.id)
+        console.log(crystal, 'lets see');
+        res.render('crystals/show', {
+            title: 'MORE ABOUT: ', crystal
+     });
+    } catch(err) {
+        console.log(err);
+    }
+};
--- routes/crystals.js
+++ routes/crystals.js
@@ -2,7 +2,9 @@ var router = require('express').Router();
 const crystalCtrl = require('../controllers/crystals');
 
 router.get('/crystals', crystalCtrl.index);
-
+router.get('/crystals/new', crystalCtrl.new);
+router.post('/crystals', crystalCtrl.create);
+router.get('/crystals/:id', crystalCtrl.show);
 
 
 module.exports = router;
