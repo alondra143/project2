9361a5255181aea86853a91fc2d1c1d31b32ddb3
alondra 3 <not given>
2021-10-03 14:56:10 -0700

    add index and show functionality for user's collections/specific collection

--- controllers/collections.js
+++ controllers/collections.js
@@ -3,13 +3,39 @@ const Collection = require('../models/collection')
 
 module.exports = {
     create,
+    index,
+    show,
 }
 function create(req, res) {
     const collection = new Collection(req.body)
     collection.userId = req.user._id;
     collection.userName = req.user.name;
+    collection.save(function(err) {
+        if(err) console.log(err)
+    })
     console.log(collection);
  res.redirect('/crystals')
 };
-    // find the user document we want to add the collection to (req.user)?
-    // we want to push the user's id into the belongsTo property of the crystal collection
+
+
+async function index(req, res) {
+    try {
+        const collections = await Collection.find({userId: req.user._id});
+        res.render('collections/index', {
+            collections, title: 's Crystal Cluster',
+        })
+        console.log(collections);
+    } catch(err) {
+        res.send(err);
+    }
+}
+
+async function show(req, res) {
+    try {
+        const collection = await Collection.findById(req.params.id);
+        res.render('collections/show', {
+            collection})
+    } catch(err) {
+        res.send(err);
+    }
+}
--- routes/collections.js
+++ routes/collections.js
@@ -1,7 +1,9 @@
 var router = require('express').Router();
 const collectionCtrl = require('../controllers/collections');
 
-router.post('/crystals/:id/collections', collectionCtrl.create);
+router.get('/collections/:id', collectionCtrl.show);
+router.post('/crystals/:id/collection', collectionCtrl.create);
+router.get('/crystals/:id/collections', collectionCtrl.index);
 
 
 module.exports = router;
