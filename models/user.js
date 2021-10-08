991f9e09bf7523504ec45648d32e23be7d68cfcb
alondra 3 <not given>
2021-10-07 22:39:20 -0700

    clean up code

--- models/user.js
+++ models/user.js
@@ -2,12 +2,6 @@ const mongoose = require('mongoose');
 
 // one collection has many crystals
 // user has many collections, a collection belongs to a user
-const collectionSchema = new mongoose.Schema({
-    name: String,
-    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    userName: String,
-    crystalsAdded: [{type: mongoose.Schema.Types.ObjectId, ref: 'Crystal'}]
-});
 
 // Create your User Model
 const userSchema = new mongoose.Schema({
